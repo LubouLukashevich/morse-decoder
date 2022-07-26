4d49a30efb2a175bc90df66d8cbbb53ec9ace5e4
Lubou Lukashevich <lukashevich.rs@gmail.com>
2022-07-26 21:08:58 +0300

    init: algoritmic case 4

--- src/index.js
+++ src/index.js
@@ -38,9 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let text = expr.split('**********');
+    let result = [];
+    for (let i = 0; i < text.length; i++) {
+        result[i] = '';
+        let word = text[i];
+        for (let j = 0; j < word.length; j+=10) {
+            let word2 = text[i];
+            let letter = word2.slice(j, j + 10);
+            let new_word = '';
+            for (let k = 0; k < 10; k+=2) {
+                if (letter[k] === '1') { (letter[k + 1] === '0') ? new_word += '.' : new_word += '-';}
+            };
+            if (new_word in MORSE_TABLE) {result[i] += MORSE_TABLE[new_word]}
+            }
+        }
+    return result.join(' ');
+    }
+    
 
 module.exports = {
     decode
 }
+
+console.log(module.exports.decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010'));
